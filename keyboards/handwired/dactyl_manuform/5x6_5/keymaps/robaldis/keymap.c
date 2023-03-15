f98a8ceb9207097b96e384f54652a26be45dd9ea
robert.aldis <robert.aldis@finbourne.com>
2023-03-15 09:59:42 +0000

    Reset programming row to normal

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/robaldis/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/robaldis/keymap.c
@@ -32,12 +32,12 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT_5x6_5(
-     KC_GRV,          KC_PAST, KC_LBRC, KC_LCBR, KC_LPRN, KC_HASH,                                 KC_CIRC,KC_RPRN,KC_RCBR,      KC_RBRC,KC_DLR,  KC_MINS,
+     KC_GRV,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                    KC_6,   KC_7,   KC_8,         KC_9,   KC_0,    KC_MINS,
      KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                    KC_Y,   KC_U,   KC_I,         KC_O,   KC_P,    KC_BSLS,
      KC_ESC,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                    KC_H,   KC_J,   KC_K,         KC_L,   KC_SCLN, KC_QUOT,
      LSFT_T(KC_NUHS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                    KC_N,   KC_M,   KC_COMM,      KC_DOT, KC_SLSH, OSM(MOD_RSFT),
-                             KC_LGUI, KC_LALT, MO(MUSIC), LT(MOUSE, KC_SPACE), KC_DEL,               LT(ARROW, KC_ENT),   LT(FUN, KC_TAB), LT(NUM, KC_BSPC), KC_LALT,    KC_LGUI,
-                                                     KC_LCTL, MO(SHIFT),              MO(SHIFT), KC_RCTL
+                      KC_LGUI, KC_LALT, MO(MUSIC), LT(MOUSE, KC_SPACE), LT(ARROW, KC_DEL),               LT(ARROW, KC_ENT),   LT(FUN, KC_TAB), LT(NUM, KC_BSPC), KC_LALT,    KC_LGUI,
+                                                     KC_LCTL, KC_LSFT,              KC_LSFT, KC_RCTL
   ),
 
   [SHIFT] = LAYOUT_5x6_5(
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [ARROW] = LAYOUT_5x6_5(
-     KC_GRV,        KC_PAST, KC_LBRC, KC_LCBR, KC_LPRN, KC_HASH,                              KC_CIRC,KC_RPRN,KC_RCBR,      KC_RBRC,KC_DLR,         KC_MINS,
+     KC_GRV,          KC_PAST, KC_LBRC, KC_LCBR, KC_LPRN, KC_HASH,                               KC_CIRC,KC_RPRN,KC_RCBR,      KC_RBRC,KC_DLR,  KC_MINS,
      KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                    KC_Y,   KC_U,   KC_I,         KC_O,   KC_P,           KC_BSLS,
      KC_ESC,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                    KC_LEFT,   KC_DOWN,   KC_UP,  KC_RGHT,   KC_SCLN,        KC_QUOT,
      LSFT_T(KC_NUHS),       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                    KC_N,   KC_M,   KC_COMM,      KC_DOT, CTL_T(KC_SLSH), OSM(MOD_RSFT),
